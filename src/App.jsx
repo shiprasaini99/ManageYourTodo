8f1d032ad3265e3d96dab9a155b623eb9c2c974d
sainishipra99 <shiprasaini2012@gmail.com>
2024-09-25 01:47:05 +0530

    updated code

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import { useState } from 'react'
 import './App.css'
 import { ToDoProvider } from './contexts';
+import { useEffect } from 'react';
 
 function App() {
   const [todos, setTodos] = useState([]);
+
+
   const addTodo = (todo) =>{
     setTodos((prev)=>[{id:Date.now(), ...todo}, ...prev]);
   }
@@ -20,6 +23,18 @@ function App() {
   const toggleComplete = (id) => {
     setTodos((prev) => prev.map((prevTodo) => prevTodo.id ===id ? {...prevTodo, completed: !prevTodo.completed} : prevTodo)); 
   }
+
+  useEffect(()=>{
+    const todos = JSON.parse(localStorage.getItem("todos"));
+    if(todos && todos.length > 0){
+      setTodos(todos);
+    };
+
+  },[])
+
+  useEffect(()=>{
+    localStorage.setItem("todos", JSON.stringify(todos));
+  },[todos])
 
   return (
     <ToDoProvider value={{todos, addTodo, deleteTodo, toggleComplete, updateTodo}}>
